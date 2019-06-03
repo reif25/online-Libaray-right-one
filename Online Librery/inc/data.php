aa979ffd6a87eb5fddc1fa8a1f9f8c7a9a5ccd6b
reif25 <43653331+reif25@users.noreply.github.com>
2019-06-03 18:26:46 -0400

    Add files via upload

---
+++ Online Librery/inc/data.php
@@ -0,0 +1,166 @@
+<?php
+$catalog = [];
+
+//Books
+$catalog[101] = [
+	"title" => "The Cat in the Hat",
+	"img" => "img/media/cat.jpg",
+    "genre" => "Children Fiction",
+    "format" => "Hardcover",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Dr. Suess"
+    ],
+    "publisher" => "Dr. Seuss",
+    "isbn" => '9781571352255'
+];
+$catalog[102] = [
+    "title" => "OF Mice And Men",
+    "img" => "img/media/ofmice.jpg",
+    "genre" => "Children's Fiction",
+    "format" => "Hardcover",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "John Steinbeck"
+    ],
+    "publisher" => "Penguin Random House LLC.",
+    "isbn" => '9781590542903'
+];
+$catalog[103] = [
+    "title" => "Frankinstein",
+    "img" => "img/media/frank.jpg",
+    "genre" => " Fiction",
+    "format" => "Paperback",
+    "year" => 1978,
+    "category" => "Books",
+    "authors" => [
+        "Mary Shelley"
+    ],
+    "publisher" => "",
+    "isbn" => '9786077627128'
+];
+$catalog[104] = [
+    "title" => "Avatar",
+    "img" => "img/media/New.jpg",
+    "genre" => "Didactic Fiction",
+    "format" => "Paperback",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "James Cameron"
+    ],
+    "publisher" => "Barnes & Noble",
+    "isbn" => '9789687277493'
+];
+
+//Movies
+$catalog[201] = [
+    "title" => "Fate of the furious",
+    "img" => "img/media/fate.jpg",
+    "genre" => "Action",
+    "format" => "DVD, Digital",
+    "year" => 2006,
+    "category" => "Movies",
+    "director" => "Edward Zwick",
+    "writer" => [
+        "	Chris Morgan",
+    ],
+    "stars" => [
+        "The Rock"
+        
+    ]
+];
+$catalog[202] = [
+    "title" => "Get Rich Or Die Trying",
+    "img" => "img/media/getrich.jpg",
+    "genre" => "action crime",
+    "format" => "DVD, Digital",
+    "year" => 2005,
+    "category" => "Movies",
+    "director" => "50 Cent",
+    "writers" => [
+        "Jim Sheridan",
+				
+    ],
+    "stars" => [
+			"50 Cents"
+			
+    ]
+];
+$catalog[203] = [
+    "title" => "Rush Hour.",
+    "img" => "img/media/Rushhour.jpg",
+    "genre" => "Action comedy",
+    "format" => "DVD, Digital",
+    "year" => 2001,
+    "category" => "Movies",
+    "director" => "Pete Docter",
+    "writer" => [
+        "Pete Docter",
+    ],
+    "stars" => [
+			"Chris Tucker",
+			"Jackie Chan"
+
+    ]
+];
+$catalog[204] = [
+    "title" => "Game Of Thrones",
+		"img" => "img/media/game.jpg",
+	  "genre" => "Animated",
+	  "format" => " Digital",
+	  "year" => 2016,
+	  "category" => "Movies",
+	  "director" => "",
+	  "writer" => [
+	      "Stan Lee"
+	  ],
+	  "stars" => [
+	      "Shameik Moore",
+	      "Hailee Steinfeld",
+	      "Jake Johnson"
+	  ]
+	];
+
+//Music
+$catalog[301] = [
+	"title" => "Usher ",
+	"img" => "img/media/usher.jpg",
+	"genre" => "Hip-Hop",
+	"format" => "CD, Digital",
+	"year" => 2003,
+	"category" => "Music",
+	"artist" => "Usher"
+];
+
+$catalog[302] = [
+
+	"title" => "NBA Youngboy - until",
+	"img" => "img/media/until.jpg",
+	"genre" => "HIP-HOP",
+	"format" => " Digital",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Nba Youngboy"
+];
+$catalog[303] = [
+	"title" => "Until death",
+	"img" => "img/media/untildeath.jpg",
+	"genre" => "HIP-Pop",
+	"format" => " Digital",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Nba YoungBoy"
+];
+$catalog[304] = [
+	"title" => "Meek Mill -Championship",
+	"img" => "img/media/champions.jpg",
+	"genre" => "Rap",
+	"format" => " Digital",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Meek mill"
+];
+?>
